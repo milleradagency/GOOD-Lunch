208d8a0c496e146149d14e61690653b88e46e8d5
williampansky <williampansky@gmail.com>
2017-03-20 00:57:26 -0500

    Updates fixed

--- javascript/lunch2017.js
+++ javascript/lunch2017.js
@@ -5,7 +5,7 @@ $(document).ready(function() {
   }, 1500, function() {
     // Animation complete.
   });
-  $(".hero-nav").animate({
+  $("nav").animate({
     opacity: 1,
   }, 2000, function() {
     // Animation complete.
@@ -16,140 +16,140 @@ $(document).ready(function() {
 
   // fullPage
   // https://github.com/alvarotrigo/fullPage.js/#initialization
-  $('#fullpageIntro').fullpage({
-    // Scrolling
-    setAllowScrolling: false,
-    // Design
-    controlArrows: false
-  });
-    // un-comment to edit main site content
-    $.fn.fullpage.destroy('all');
-    $('#fullpageIntro').remove();
+  // $('#fullpageIntro').fullpage({
+  //   // Scrolling
+  //   setAllowScrolling: false,
+  //   // Design
+  //   controlArrows: false
+  // });
+  //   // un-comment to edit main site content
+  //   $.fn.fullpage.destroy('all');
+  //   $('#fullpageIntro').remove();
 });
 
 // # Neat Parallax Hero Effect
 // ## http://codepen.io/magnificode/pen/GpqGOm
-var heroShinker = function() {
-  // vars
-  var hero = $('.hero-nav'),
-    heroHeight = $('.hero-nav').outerHeight(true);
-    $(hero).parent().css('padding-top', heroHeight + 15);
-  var jumbo = $("#jumboHero");
-  var mobile = $("#topBar");
-  var nav = $("#topNav");
-  var main = $("main");
-  var mobile = $("#mobileNav");
-  var logoOld = $("#lunchLogoOld");
-  // var vw = $(hero).css('height', '100vh');
-
-  $(window).scroll(function() {
-    var scrollOffset = $(window).scrollTop();
-    if (scrollOffset < heroHeight) {
-      $(hero).css('height', (heroHeight - scrollOffset));
-    }
-
-    // add & remove .hero-nav class based on scrollOffset
-    if (scrollOffset > (heroHeight - 555)) {
-      hero.addClass('fixMe');
-    } else {
-      hero.removeClass('fixMe');
-    };
-
-    // change stats of #topNavMobileBars based on scrollOffset
-    // http://stackoverflow.com/a/8389571
-    // if (scrollOffset > (heroHeight - 585)) {
-    //   $("#topNavMobileBars").animate({
-    //     fontSize: "45px",
-    //     top: 12,
-    //   }, 300, function() {
-    //     // Animation complete.
-    //   });
-    // } else {
-    //   $("#topNavMobileBars").animate({
-    //     fontSize: "1.5rem",
-    //     top: 35,
-    //   }, 300, function() {
-    //     // Animation complete.
-    //   });
-    // };
-
-    // add another .hero-nav class based on scrollOffset
-    // if (scrollOffset > (heroHeight + 100)) {
-    //   hero.addClass('fixMeAgain');
-    // } else {
-    //   hero.removeClass('fixMeAgain');
-    // };
-
-    // add #jumboHero class based on scrollOffset
-    // if (scrollOffset > (heroHeight + 1000)) {
-    //   jumbo.addClass('hideMe');
-    // } else {
-    //   jumbo.removeClass('hideMe');
-    // };
-
-    // #mobileNav
-    // add .showMe to #mobileNav based on scrollOffset
-    if (scrollOffset > (heroHeight - 400)) {
-      mobile.addClass('showMe');
-    } else {
-      mobile.removeClass('showMe');
-    };
-
-    // #lunchLogoOld
-    // add .showMe to #lunchLogoOld based on scrollOffset
-    if (scrollOffset > (heroHeight - 575)) {
-      logoOld.addClass('showMe');
-    } else {
-      logoOld.removeClass('showMe');
-    };
-
-    // add & remove main class based on scrollOffset
-    // if (scrollOffset > (heroHeight - 580)) {
-    //   nav.removeClass('topNav-hello');
-    //   nav.addClass('topNav-bye');
-    // } else {
-    //   nav.removeClass('topNav-bye');
-    //   nav.addClass('topNav-hello');
-    // };
-
-    // add & remove #topBar class based on scrollOffset
-    // if (scrollOffset > (heroHeight - 580)) {
-    //   top.removeClass('topBar-hello');
-    //   top.addClass('topBar-bye');
-    // } else {
-    //   top.removeClass('topBar-bye');
-    //   top.addClass('topBar-hello');
-    // };
-  }); // end $(window).scroll
-}
-heroShinker();
+// var heroShinker = function() {
+//   // vars
+//   var hero = $('.hero-nav'),
+//     heroHeight = $('.hero-nav').outerHeight(true);
+//     $(hero).parent().css('padding-top', heroHeight + 15);
+//   var jumbo = $("#jumboHero");
+//   var mobile = $("#topBar");
+//   var nav = $("#topNav");
+//   var main = $("main");
+//   var mobile = $("#mobileNav");
+//   var logoOld = $("#lunchLogoOld");
+//   // var vw = $(hero).css('height', '100vh');
+//
+//   $(window).scroll(function() {
+//     var scrollOffset = $(window).scrollTop();
+//     if (scrollOffset < heroHeight) {
+//       $(hero).css('height', (heroHeight - scrollOffset));
+//     }
+//
+//     // add & remove .hero-nav class based on scrollOffset
+//     if (scrollOffset > (heroHeight - 555)) {
+//       hero.addClass('fixMe');
+//     } else {
+//       hero.removeClass('fixMe');
+//     };
+//
+//     // change stats of #topNavMobileBars based on scrollOffset
+//     // http://stackoverflow.com/a/8389571
+//     // if (scrollOffset > (heroHeight - 585)) {
+//     //   $("#topNavMobileBars").animate({
+//     //     fontSize: "45px",
+//     //     top: 12,
+//     //   }, 300, function() {
+//     //     // Animation complete.
+//     //   });
+//     // } else {
+//     //   $("#topNavMobileBars").animate({
+//     //     fontSize: "1.5rem",
+//     //     top: 35,
+//     //   }, 300, function() {
+//     //     // Animation complete.
+//     //   });
+//     // };
+//
+//     // add another .hero-nav class based on scrollOffset
+//     // if (scrollOffset > (heroHeight + 100)) {
+//     //   hero.addClass('fixMeAgain');
+//     // } else {
+//     //   hero.removeClass('fixMeAgain');
+//     // };
+//
+//     // add #jumboHero class based on scrollOffset
+//     // if (scrollOffset > (heroHeight + 1000)) {
+//     //   jumbo.addClass('hideMe');
+//     // } else {
+//     //   jumbo.removeClass('hideMe');
+//     // };
+//
+//     // #mobileNav
+//     // add .showMe to #mobileNav based on scrollOffset
+//     if (scrollOffset > (heroHeight - 400)) {
+//       mobile.addClass('showMe');
+//     } else {
+//       mobile.removeClass('showMe');
+//     };
+//
+//     // #lunchLogoOld
+//     // add .showMe to #lunchLogoOld based on scrollOffset
+//     if (scrollOffset > (heroHeight - 575)) {
+//       logoOld.addClass('showMe');
+//     } else {
+//       logoOld.removeClass('showMe');
+//     };
+//
+//     // add & remove main class based on scrollOffset
+//     // if (scrollOffset > (heroHeight - 580)) {
+//     //   nav.removeClass('topNav-hello');
+//     //   nav.addClass('topNav-bye');
+//     // } else {
+//     //   nav.removeClass('topNav-bye');
+//     //   nav.addClass('topNav-hello');
+//     // };
+//
+//     // add & remove #topBar class based on scrollOffset
+//     // if (scrollOffset > (heroHeight - 580)) {
+//     //   top.removeClass('topBar-hello');
+//     //   top.addClass('topBar-bye');
+//     // } else {
+//     //   top.removeClass('topBar-bye');
+//     //   top.addClass('topBar-hello');
+//     // };
+//   }); // end $(window).scroll
+// }
+// heroShinker();
 
 // #mobileNav controls
-$(function() {
-  // open/close mobile nav
-  $("#mobileNavBar").on("click", function() {
-    if ($("#mobileNav").hasClass("showMeMore") == false) {
-      $("#mobileNav").addClass('showMeMore');
-      $("body").addClass('noscroll');
-      $("#mobileNavBars i").toggleClass('fa-bars fa-times');
-    } else {
-      $("#mobileNav").removeClass('showMeMore');
-      $("body").removeClass('noscroll');
-      $("#mobileNavBars i").toggleClass('fa-times fa-bars');
-    };
-  });
-
-  // scrollSpy link activates mobile close
-  $(".mainShow").on("click", function() {
-    if ($("#mobileNav").hasClass("showMeMore") == true) {
-      $("#mobileNav").removeClass('showMeMore');
-      $("body").removeClass('noscroll');
-      $("#mobileNavBars i").toggleClass('fa-times fa-bars');
-    } else {
-      //
-    };
-  });
-});
+// $(function() {
+//   // open/close mobile nav
+//   $("#mobileNavBar").on("click", function() {
+//     if ($("#mobileNav").hasClass("showMeMore") == false) {
+//       $("#mobileNav").addClass('showMeMore');
+//       $("body").addClass('noscroll');
+//       $("#mobileNavBars i").toggleClass('fa-bars fa-times');
+//     } else {
+//       $("#mobileNav").removeClass('showMeMore');
+//       $("body").removeClass('noscroll');
+//       $("#mobileNavBars i").toggleClass('fa-times fa-bars');
+//     };
+//   });
+//
+//   // scrollSpy link activates mobile close
+//   $(".mainShow").on("click", function() {
+//     if ($("#mobileNav").hasClass("showMeMore") == true) {
+//       $("#mobileNav").removeClass('showMeMore');
+//       $("body").removeClass('noscroll');
+//       $("#mobileNavBars i").toggleClass('fa-times fa-bars');
+//     } else {
+//       //
+//     };
+//   });
+// });
 
 // http://stackoverflow.com/questions/7035331/prevent-automatic-browser-scroll-on-refresh/18633915#18633915
 $(window).on('beforeunload', function() {
@@ -158,31 +158,34 @@ $(window).on('beforeunload', function() {
 
 // smooth scrolling
 // forked: http://codepen.io/chriscoyier/pen/dpBMVP
-$('a[href*="#"]:not([href="#"])').click(function() {
-  if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
-    var target = $(this.hash);
-    target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
-    if (target.length) {
-      $('html, body').animate({
-        scrollTop: target.offset().top -30
-      }, 1000);
-      return false;
-    }
-  }
-});
+// $('a[href*="#"]:not([href="#"])').click(function() {
+//   if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
+//     var target = $(this.hash);
+//     target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+//     if (target.length) {
+//       $('html, body').animate({
+//         scrollTop: target.offset().top + 30
+//       }, 1000);
+//       return false;
+//     }
+//   }
+// });
 
 // prevent a#SECTION from being added to URL on click
 // http://stackoverflow.com/questions/20215248/prevent-href-link-from-changing-the-url-hash
 $('.mainShow').click(function(event){
   event.preventDefault();
 });
+$('.btn').click(function(event){
+  event.preventDefault();
+});
 
 // sponsorshipOpp tables
 // http://codepen.io/grayghostvisuals/pen/gGxfh
-function priceToggle(e) {
-  e.preventDefault();
-  $(this).parent().next().toggleClass('opened');
-}
-
-$('.price-block__trigger').on('click', priceToggle);
-$('.price-block__trigger').on('touchstart', priceToggle);
+// function priceToggle(e) {
+//   e.preventDefault();
+//   $(this).parent().next().toggleClass('opened');
+// }
+//
+// $('.price-block__trigger').on('click', priceToggle);
+// $('.price-block__trigger').on('touchstart', priceToggle);
