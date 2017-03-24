a5a4d889320ad29fe74e8495726a4ff9d2d96534
maa-wpansky <wpansky@iMac-01.local>
2017-03-24 15:26:32 -0500

    Deployment prep
    #44, #43, #42, #41, #40

--- themes/2017/static/javascript/lunch2017.js
+++ themes/2017/static/javascript/lunch2017.js
@@ -246,3 +246,14 @@ if(/(iPhone|iPad|iPod)\sOS\s9/.test(navigator.userAgent)) {
     bar.css("height", "50px");
     bar.css("border-bottom", "1px solid #595959");
 };
+
+
+// open socials in small window modal
+// http://stackoverflow.com/a/23309862
+// $(document).ready(function() {
+//   $('.facebook').click(function(e) {
+//     e.preventDefault();
+//     window.open($(this).attr('href'), 'fbShareWindow', 'height=450, width=550, top=' + ($(window).height() / 2 - 275) + ', left=' + ($(window).width() / 2 - 225) + ', toolbar=0, location=0, menubar=0, directories=0, scrollbars=0');
+//     return false;
+//   });
+// });
